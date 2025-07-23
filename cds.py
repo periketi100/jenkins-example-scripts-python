5a1f7365fe1545d382b0a8c2996a2068b4362be3
Prasad Periketi <32905394+periketi100@users.noreply.github.com>
2025-07-23 18:42:53 +0530

    Create cds.py

---
+++ cds.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+bearer_token = "ZWQ4M2U5OTUtNDkyZS00MzY3LTg3YmMtNjFiM2RjZmU0OTVi"
+
+headers = {"Authorization": "Bearer {}".format(bearer_token)}
+
+response = requests.get(
+    "https://console.dms.ort.usw2.ficoanalyticcloud.com/com.fico.dmp.manager/rest/dmp/runtime/solutions/d0ny74319/services?env=design&type=kafka",
+    headers=headers,
+)
+
+# Get the JSON response as a Python dict
+data = response.json()
+
+# Pretty-print JSON with indentation
+pretty_json = json.dumps(data, indent=4)
+
+print(pretty_json)
