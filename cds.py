0fb04256f40f1c97f5ecea9bba0c9b35e27e1a46
Prasad Periketi <32905394+periketi100@users.noreply.github.com>
2025-07-23 19:01:45 +0530

    Update cds.py

--- cds.py
+++ cds.py
@@ -1,18 +1,25 @@
-import httpx
+import urllib3
 import json
 
 bearer_token = "ZWQ4M2U5OTUtNDkyZS00MzY3LTg3YmMtNjFiM2RjZmU0OTVi"
 
-headers = {"Authorization": f"Bearer {bearer_token}"}
+http = urllib3.PoolManager()
 
 url = "https://console.dms.ort.usw2.ficoanalyticcloud.com/com.fico.dmp.manager/rest/dmp/runtime/solutions/d0ny74319/services?env=design&type=kafka"
 
-response = httpx.get(url, headers=headers)
+headers = {
+    "Authorization": f"Bearer {bearer_token}"
+}
 
-# Get the JSON response as a Python dict
-data = response.json()
+response = http.request(
+    "GET",
+    url,
+    headers=headers
+)
 
-# Pretty-print JSON with indentation
+# response.data is bytes, decode it
+data = json.loads(response.data.decode("utf-8"))
+
+# Pretty print JSON
 pretty_json = json.dumps(data, indent=4)
-
 print(pretty_json)
