c554ddd61b9577d9c029b79952089fdee566f0f6
Prasad Periketi <32905394+periketi100@users.noreply.github.com>
2025-07-23 18:59:56 +0530

    Update cds.py

--- cds.py
+++ cds.py
@@ -1,14 +1,13 @@
-import requests
+import httpx
 import json
 
-bearer_token = "OWY0YjJkYTgtZWU5ZC00ZTk5LTk4NzQtMjA3YTJjODA0NzA2"
+bearer_token = "ZWQ4M2U5OTUtNDkyZS00MzY3LTg3YmMtNjFiM2RjZmU0OTVi"
 
-headers = {"Authorization": "Bearer {}".format(bearer_token)}
+headers = {"Authorization": f"Bearer {bearer_token}"}
 
-response = requests.get(
-    "https://console.dms.ort.usw2.ficoanalyticcloud.com/com.fico.dmp.manager/rest/dmp/runtime/solutions/d0ny74319/services?env=design&type=kafka",
-    headers=headers,
-)
+url = "https://console.dms.ort.usw2.ficoanalyticcloud.com/com.fico.dmp.manager/rest/dmp/runtime/solutions/d0ny74319/services?env=design&type=kafka"
+
+response = httpx.get(url, headers=headers)
 
 # Get the JSON response as a Python dict
 data = response.json()
